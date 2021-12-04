e0ca2440b2278d440a90a2849ac2d34cfefc6fb6
dashaartiuhova <21dashka21@gmail.com>
2021-12-04 13:10:42 +0200

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -172,6 +172,29 @@ $(document).ready(function() {
             centerMode:true,
             slidesToShow: 5,
             slidesToScroll: 1,
+            responsive: [
+                {
+                  breakpoint: 992,
+                  settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true,
+                  }
+                },
+                {
+                  breakpoint: 600,
+                  settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                  }
+                },
+                {
+                  breakpoint: 470,
+                  settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                  }
+                }]
           });
       
    
